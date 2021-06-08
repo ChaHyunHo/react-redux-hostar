c24d7d9c6824a4341819293c37c6d628cea71208
chahyeonho <chamym@naver.com>
2021-06-08 23:29:43 +0900

    redux 적용

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,26 +1,42 @@
+import {createAction, handleActions} from 'redux-actions'
+
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE"; // 모듈 이름 / 액션 이름
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// 예전방식
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// redux-actions 적용
+
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1}),
+        [DECREASE]: (state, action) => ({ number: state.number - 1}),
+    },
+    initialState,
+);
 
 export default counter;
